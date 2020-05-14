84e7536e96e947e7b6bd3486fc0be9f396a60c49
hoogalei <hoogalei@gmail.com>
2020-05-14 22:49:41 +0200

    Trying to take picture

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/image_input.dart';
+
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
 
@@ -8,6 +10,8 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,16 +22,36 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
             title: Text('Add a New Place'),
           ),
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text('User Inputs...'),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Title',
+                          ),
+                          controller: _titleController,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        ImageInput(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               RaisedButton.icon(
                 icon: Icon(Icons.add),
                 label: Text('Add Place'),
                 onPressed: () {},
                 elevation: 0,
                 materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                color: Theme.of(context).accentColor,
               )
             ],
           ),
